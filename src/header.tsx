1da0e40ec42c1fec75db107a102754723b7da830
Yujin <ylim.0348@gmail.com>
2026-01-23 21:29:51 -0700

    fixed import useState

--- src/header.tsx
+++ src/header.tsx
@@ -1,10 +1,3 @@
-import { useState } from 'react';
-// useState is how you store values or data, and you use this instead of let color="white".
-// How? You import it like above and then do: const[color, setColor] = useState('white');
-// you can also keep the initial empty to map new data from rest API.
-// You use useState because when you setColor, it renders the whole page again without having to refresh. 
-
-
 function Header() {
 
 
--- src/todoPage.tsx
+++ src/todoPage.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+// useState is how you store values or data, and you use this instead of let color="white".
+// How? You import it like above and then do: const[color, setColor] = useState('white');
+// you can also keep the initial empty to map new data from rest API.
+// You use useState because when you setColor, it renders the whole page again without having to refresh. 
 
 function TodoPage() {
-  const [btnText, setBtnText] = useState("click me")
+  const [btnText, setBtnText] = useState("click me");
   
   const clicked = async () => {
-    setBtnText("You clicked the button!")
+    setBtnText("You clicked the button!");
     const [tabs] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tabs.id! },
@@ -12,7 +16,7 @@ function TodoPage() {
         alert("Hello from your Chrome extension!");
       },
     });
-  };
+  }
 
   return (
     <main>
